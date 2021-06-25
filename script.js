41dee5020fc4e7686ea4a1a03b73737c0127e59e
Eddie Yeung <eycs0317@gmail.com>
2021-06-25 15:43:43 -0700

    working on display questions and correct/wrong answer was pick

--- script.js
+++ script.js
@@ -11,27 +11,35 @@ let scoreboard = {
   player1: 10,
 }
 let startTime = 101;
+let userAnswer = null;
+let questionNum = 0;
 
 
 //event
-startButton.addEventListener('click', timer);
+startButton.addEventListener('click', () => {
+  timer();
+  displayQuestion();
+  questionElement.style.display = 'block';
+  timerElement.style.display = 'block';
+  headerElement.style.display = 'none'
+});
 
 
 const questions = [
   {
     question: 'what is the letter?',
     choice: ['a', 'b', 'c','d' ],
-    answer: 'a'
+    answer: '0'
   },
   {
     question: 'what is the number?',
     choice: ['1', '2', '3','4' ],
-    answer: '2'
+    answer: '1'
   },
   {
     question: 'what is the color?',
     choice: ['red', 'blue', 'yellow','black' ],
-    answer: 'yellow'
+    answer: '2'
   }
 ]
 
@@ -47,18 +55,36 @@ function timer() {
   var intervalKey = setInterval(() => {
     startTime--;
     time.innerText = startTime
-    if(startTime === 0) {
+    if(startTime <= 0) {
       clearInterval(intervalKey);
     }
   },1000)
 }
 //display questions
 function displayQuestion() {
-  question.innerText = questions[0].question
-  questions[0].choice.forEach(ans => {
+
+  question.innerText = questions[questionNum].question
+  questions[questionNum].choice.forEach((element, index) => {
   let li = document.createElement('li');
-  li.innerText = ans;
+  li.innerText = element;
+  li.setAttribute('name', index)
   ul.appendChild(li)
+
+  li.addEventListener('click', () => {
+    userAnswer = li.getAttribute('name')
+    console.log(typeof userAnswer)
+    console.log('questions[questionNum].answer', typeof questions[questionNum].answer)
+    if(userAnswer === questions[questionNum].answer) {
+      console.log('correct')
+    } else {
+      // - 10 sec from timer
+      console.log('wrong')
+    }
+    questionNum++;
+
+  })
+
+
 })
 }
 
@@ -71,3 +97,6 @@ function gameOver() {
 
 
 
+
+//work
+// questionElement.style.display = 'none'
