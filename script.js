5dd09ea24679c4bc334b0036140137e74435fe4f
Eddie Yeung <eycs0317@gmail.com>
2021-06-27 15:08:57 -0700

    working on input name and score data to local storage

--- script.js
+++ script.js
@@ -11,36 +11,49 @@ let showScore = document.querySelector('.show-score')
 let submitButton = document.querySelector('#submit')
 let userName = document.querySelector('#name')
 let showAnswer = document.querySelector('.show-answer')
-let scoreboard = {
-  player1: 10,
-}
+let nameAndScore = document.querySelector("name-and-score")
+let scoreboard = [];
 let currentScore = 0;
-let startTime = 101;
+let startTime = 51;
 let userAnswer = null;
 let questionNum = 0;
 
 
 //event
-startButton.addEventListener('click', () => {
-  start();
-});
+startButton.addEventListener('click', start)
+submitButton.addEventListener('click', submitName)
 
 
 const questions = [
   {
-    question: 'what is the letter?',
-    choice: ['a', 'b', 'c','d' ],
+    question: '1. Which one is NOT a JavaScript variable?',
+    choice: ['1. str', '2. const', '3. var','4. let' ],
     answer: '0'
   },
   {
-    question: 'what is the number?',
-    choice: ['1', '2', '3','4' ],
-    answer: '1'
+    question: '2. Which one is NOT a JavaScript array method?',
+    choice: ['1. shift()', '2. Array.isArray()', '3. split()','4. pop()' ],
+    answer: '2'
   },
   {
-    question: 'what is the color?',
-    choice: ['red', 'blue', 'yellow','black' ],
-    answer: '2'
+    question: '3. What does 2 + "7" return?',
+    choice: ['1. "27"', '2. 9', '3. 27','4. "9"' ],
+    answer: '0'
+  },
+  {
+    question: '4. Which one is Falsy Values?',
+    choice: ['1. {}', '2. []', '3. -1','4. -0' ],
+    answer: '3'
+  },
+  {
+    question: '5. What does ("3" == 3) return?',
+    choice: ['1. true', '2. false'],
+    answer: '0'
+  },
+  {
+    question: '6. What does (!!true) return?',
+    choice: ['1. undefined', '2. true', '3. false','4. I don\'t know' ],
+    answer: '1'
   }
 ]
 
@@ -61,8 +74,9 @@ function timer() {
   var intervalKey = setInterval(() => {
     startTime--;
     time.innerText = startTime
-    if(startTime <= 0) {
+    if(startTime <= -1) {
       clearInterval(intervalKey);
+      gameOver()
     }
   },1000)
 }
@@ -83,60 +97,71 @@ function displayQuestion() {
     li.addEventListener('click', () => {
       userAnswer = li.getAttribute('index')
       if(userAnswer === questions[questionNum].answer) {
-        console.log('correct')
         currentScore+=1
-        // ul.innerHTML = ''
-        console.log(showAnswer.innerText)
+        ul.innerHTML = ''
         showAnswer.innerText = 'Correct!!'
         clearMessage()
       } else {
-        console.log('wrong')
         startTime-=10;
-        // ul.innerHTML = ''
+        ul.innerHTML = ''
         showAnswer.innerText = 'Wrong!!'
         clearMessage()
       }
       questionNum++;
       setTimeout(displayQuestion,1000)
-      console.log('currentScore', currentScore)
+
     })
-
-
   })
   }
-
 }
 
 //when are questions are answer or time is over
 function gameOver() {
-  console.log('no more questions')
   questionElement.style.display = 'none';
   timerElement.style.display = 'none';
   doneElement.style.display = 'block'
-  showScore.innerText = `Your final Score is ${currentScore}/3`
+  showScore.innerText = `Your final Score is ${currentScore}/${questions.length}`
 }
 
 //function for the username to submit name
 function submitName(e) {
   e.preventDefault();
   if(userName.value === '') {
-    console.log('no name was input')
+    alert('Please input Name')
   } else {
-    scoreboard[userName.value] = currentScore;
-    localStorage.setItem('score', JSON.stringify(scoreboard))
-    doneElement.style.display = 'none';
-    scoreboardElement.style.display = 'block'
+    var data = JSON.parse(localStorage.getItem('data'));
+    if(!data) {
+      console.log('no data')
+      scoreboard.push({name:userName.value, score: currentScore})
+      console.log('here')
+      localStorage.setItem('data', JSON.stringify(scoreboard))
+    } else {
+      data.push({name:userName.value, score: currentScore})
+      localStorage.setItem('data', JSON.stringify(data))
+      console.log('data', data)
+    }
+    // data.push({name:userName.value, score: currentScore});
+    // console.log(data)
+    // localStorage.setItem('score', JSON.stringify(scoreboard))
+    // scoreboardElement.style.display = 'block'
+    // doneElement.style.display = 'none';
+    // displayUsernameAndScore();
   }
 }
-submitButton.addEventListener('click', submitName)
+
 
 //function to clear the correct/wrong message
 function clearMessage() {
   // setTimeout(() => showAnswer.innerText = '',1000)
   setTimeout(() => {
-    ul.innerHTML = ''
     showAnswer.innerText = ''
   },1000)
 }
-//work
-// questionElement.style.display = 'none'
+
+//display username and score
+function displayUsernameAndScore() {
+  var data = JSON.parse(localStorage.getItem('score'))
+
+
+  // nameAndScore.innerText = `${} ${}`
+}
